98ee8afb26752ac0a6be7cb1b9618491301ce10c
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2023-08-06 23:19:49 -0400

    Added some UI elements

--- mainui.py
+++ mainui.py
@@ -10,9 +10,11 @@ class App(ctk.CTk):
 
         self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
         self.overrideredirect(True)
+        ctk.set_default_color_theme("green")
 
-        self.navbar = NavBar(root=self, width=self.WIDTH, height=self.HEIGHT / 10)
-        self.navbar.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
+        self.navbar = NavBar(root=self, width=self.WIDTH, height=self.HEIGHT / 10, fg_color="#1f1b1b")  # #242424
+        self.navbar.grid(row=0, column=1, padx=0, pady=0, sticky="nsew")
+        self.navbar.columnconfigure(0, weight=1)
 
 
 # --------------------- NavBar ---------------------
@@ -22,8 +24,15 @@ class NavBar(ctk.CTkFrame):
 
         self.root = root
 
-        self.button = ctk.CTkButton(self.root, text="Quit", command=self.root.quit)
-        self.button.grid(row=0, column=0, padx=20)
+        self.title = ctk.CTkLabel(self.root, text="SFXDownloader", font=('Helvetica', 18, 'bold'),
+                                  height=self.root.HEIGHT / 10, width=600,
+                                  bg_color="#1f1b1b")
+        self.title.grid(row=0, column=0, padx=0, sticky=ctk.W + ctk.E)
+
+        self.button = ctk.CTkButton(self.root, text="X", font=('Helvetica', 18, 'bold'), command=self.root.quit,
+                                    fg_color="#1f1b1b", hover_color="darkred", height=self.root.HEIGHT / 10,
+                                    width=self.root.HEIGHT / 10)
+        self.button.grid(row=0, column=1, padx=0, sticky=ctk.W)
 
 
 if __name__ == "__main__":
