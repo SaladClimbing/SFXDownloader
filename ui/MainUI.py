f42e6aba64e1d31883a33260ae59c8934898b8b9
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2023-08-08 00:19:17 -0400

    Functional UI now where you can download .mp3's from YouTube videos

--- ui/MainUI.py
+++ ui/MainUI.py
@@ -7,20 +7,26 @@ from ui.SelectorPanel import SelectorPanel
 # --------------------- Window ---------------------
 class App(ctk.CTk):
     def __init__(self):
-        super().__init__()
+        super().__init__()  # Initialize the window
+
+        # Dimensions
         self.WIDTH = 800
         self.HEIGHT = 600
 
         self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+
+        # Disable the toolbar and change to green theme
         self.overrideredirect(True)
         ctk.set_default_color_theme("green")
 
-        self.navbar = NavBar(root=self, width=self.WIDTH, height=self.HEIGHT / 20, fg_color="#1f1b1b",
-                             border_width=2)
-        self.navbar.pack(side=ctk.TOP)
+        # Sections setup
+        self.navbar_height = self.HEIGHT / 20
+        self.navbar = NavBar(root=self, width=self.WIDTH, height=self.navbar_height, fg_color="#1f1b1b",
+                             border_width=2) \
+            .pack(side=ctk.TOP)
 
-        self.selector_panel = SelectorPanel(root=self, width=self.WIDTH / 2, height=self.HEIGHT - self.HEIGHT / 20)
-        self.selector_panel.pack(side=ctk.LEFT)
+        self.selector_panel = SelectorPanel(root=self, width=self.WIDTH / 2, height=self.HEIGHT - self.navbar_height) \
+            .pack(side=ctk.LEFT)
 
 
 if __name__ == "__main__":
--- ui/SelectorPanel.py
+++ ui/SelectorPanel.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 
+from downloader import download
+
 
 # --------------------- Selector Panel ---------------------
 class SelectorPanel(ctk.CTkFrame):
@@ -15,8 +17,12 @@ class SelectorPanel(ctk.CTkFrame):
 
     def pack_search(self):
         self.search_box = ctk.CTkEntry(self, height=25, width=200, font=('Helvetica', 18, 'bold'),
-                                       placeholder_text="Insert Link Here") \
-            .pack(padx=100)
+                                       placeholder_text="Insert Link Here")
+        self.search_box.pack(padx=100, pady=5)
 
-        self.search_button = ctk.CTkButton(self, height=25, width=200, text="Search", font=('Helvetica', 18, 'bold')) \
-            .pack(padx=100, pady=10)
+        self.search_button = ctk.CTkButton(self, height=25, width=200, text="Search", font=('Helvetica', 18, 'bold'),
+                                           command=self.download) \
+            .pack(padx=100, pady=5)
+
+    def download(self):
+        download(self.search_box.get(), False)
