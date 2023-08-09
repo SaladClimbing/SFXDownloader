d853f4bb3670fcbd79b1faa1d7d9e95d61cd87ba
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2023-08-09 09:29:55 -0400

    Added idea for resizing thumbnail

--- ui/DownloadInfo.py
+++ ui/DownloadInfo.py
@@ -14,10 +14,12 @@ class DownloadInfo(ctk.CTkFrame):
         self.HEIGHT = kwargs['height']
         self.WIDTH = kwargs['width']
 
-        self.pack_thumbnail("LINKHERE")
+        self.pack_thumbnail(
+            "https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1280,h_720/https://blog.snappa.com/wp-content/uploads/2019/01/YouTube-Thumbnail-Dimensions.jpg")
 
     def pack_thumbnail(self, url):
         img_data = requests.get(url).content
-        img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-        self.thumbnail = ctk.CTkLabel(self, image=img)
+        unedited_img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+        img = unedited_img.resize((self.WIDTH, self.HEIGHT * 9 / 16))  # TODO FIX RESIZE
+        self.thumbnail = ctk.CTkLabel(self, image=img, width=self.WIDTH, height=self.WIDTH * 9 / 16)
         self.thumbnail.pack()
