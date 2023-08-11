57bf049201092388cf632f7965a9432685b11c49
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2023-08-11 16:20:01 -0400

    Added details to the video and some more functionality

--- ui/DownloadInfo.py
+++ ui/DownloadInfo.py
@@ -14,8 +14,24 @@ class DownloadInfo(ctk.CTkFrame):
 
         self.pack_thumbnail(
             "")
+        self.pack_details()
 
     def pack_thumbnail(self, url):
         img = get_resized_thumbnail(url, self.WIDTH)
-        self.thumbnail = ctk.CTkLabel(self, image=img, width=self.WIDTH, height=self.WIDTH * 9 / 16, text="")
+        self.thumbnail = ctk.CTkLabel(self, image=img, width=self.WIDTH - 25, height=(self.WIDTH - 25) * 9 / 16,
+                                      text="")
         self.thumbnail.pack(padx=25, pady=10)
+
+    def pack_details(self):
+        self.line_frame = ctk.CTkFrame(self, width=2, height=100, bg_color="white")
+        self.line_frame.pack(padx=(25, 5), side=ctk.LEFT)
+
+        self.title_label = ctk.CTkLabel(self, width=self.WIDTH - 25, font=('Helvetica', 18, 'bold'), justify="left",
+                                        anchor="w", text="Title: ")
+        self.title_label.pack()
+        self.author_label = ctk.CTkLabel(self, width=self.WIDTH - 25, font=('Helvetica', 18, 'bold'), justify="left",
+                                         anchor="w", text="Author: ")
+        self.author_label.pack()
+        self.time_label = ctk.CTkLabel(self, width=self.WIDTH - 25, font=('Helvetica', 18, 'bold'), justify="left",
+                                       anchor="w", text="Duration: ")
+        self.time_label.pack()
--- ui/SelectorPanel.py
+++ ui/SelectorPanel.py
@@ -1,6 +1,9 @@
+import tkinter as tk
+from datetime import timedelta
 from threading import Thread
 
 import customtkinter as ctk
+from pytube import YouTube
 
 from downloader import download, search
 from ui.ImageManipulation import get_resized_thumbnail
@@ -29,17 +32,17 @@ class SelectorPanel(ctk.CTkFrame):
         self.search_box.pack(padx=100, pady=5)
 
         self.search_button = ctk.CTkButton(self, height=25, width=200, text="Search", font=('Helvetica', 18, 'bold'),
-                                           command=lambda: self.change_thumbnail(self.search_box.get()))
+                                           command=self.search_button_clicked)
         self.search_button.pack(padx=100, pady=5)
 
     def pack_options(self):
         self.keep_video = ctk.CTkCheckBox(self, height=25, width=200, font=('Helvetica', 18, 'bold'),
-                                          text="Keep video file")
+                                          text="Keep video file", state=tk.DISABLED)
         self.keep_video.pack(padx=100, pady=25)
 
     def pack_download(self):
         self.download_button = ctk.CTkButton(self, height=25, width=200, font=('Helvetica', 18, 'bold'),
-                                             text="Download", command=self.download)
+                                             text="Download", state=tk.DISABLED, command=self.download)
         self.download_button.pack(padx=100, pady=50)
 
     # </editor-fold>
@@ -51,9 +54,22 @@ class SelectorPanel(ctk.CTkFrame):
         download_thread.start()
 
     def search_button_clicked(self):
-        self.change_thumbnail(self.search_box.get())
+        url = self.search_box.get()
+        self.change_thumbnail(url)
+        self.video = YouTube(url)
+        self.change_details(self.video)
+        self.enable_buttons()
 
     def change_thumbnail(self, url):
         self.download_info.thumbnail.configure(image=get_resized_thumbnail(search(url), self.download_info.WIDTH))
+
+    def change_details(self, video: YouTube):
+        self.download_info.title_label.configure(text=f"Title: {video.title}")
+        self.download_info.author_label.configure(text=f"Author: {video.author}")
+        self.download_info.time_label.configure(text=f"Duration: {str(timedelta(seconds=video.length))}")
+
+    def enable_buttons(self):
+        self.keep_video.configure(state=tk.NORMAL)
+        self.download_button.configure(state=tk.NORMAL)
 
     # </editor-fold>
